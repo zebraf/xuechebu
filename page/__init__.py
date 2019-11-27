ed7ea143c5135038594f6efd86389c516226b977
Zebrafish <1371201997@qq.com>
2019-11-27 10:54:12 +0800

    昵称数据

--- page/__init__.py
+++ page/__init__.py
@@ -9,3 +9,4 @@ name = By.XPATH, '//*[contains(@text,"请输入手机")]'  # 账号
 pwd = By.ID, 'com.bjcsxq.chat.carfriend:id/login_pwd_et'  # 密码
 login_btn = By.ID, 'com.bjcsxq.chat.carfriend:id/login_btn'  # 登录按钮
 com_btn = By.XPATH, '//*[contains(@text,"确定")]'  # 登录提示框确定按钮
+nick_name = By.ID, 'com.bjcsxq.chat.carfriend:id/mine_username_tv' # 昵称
