a00a4549d36523664d86439d5012b12832ba8725
Zebrafish <1371201997@qq.com>
2019-11-27 11:52:13 +0800

    获取昵称

--- page/login_page.py
+++ page/login_page.py
@@ -10,9 +10,13 @@ class LoginPage(BasePage):
     pwd = page.pwd
     login_btn = page.login_btn
     com_btn = page.com_btn
+    self.nick_name = page.nick_name
 
     def login_func(self, name, pwd):
         self.input_func(self.name, name)
         self.input_func(self.pwd, pwd)
         self.click_func(self.login_btn)
         self.click_func(self.com_btn)
+
+    def get_nick_name(self):
+        return self.find_ele(self.nick_name)
